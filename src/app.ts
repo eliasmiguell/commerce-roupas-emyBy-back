ed0375ab260c260d72e99169cdc44d4f687dd8bc
eliamiguel <eliasperreiramiguel@gmail.com>
2025-09-13 13:48:00 -0300

    Primeiro commit no novo GitHub

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,10 @@ const PORT = process.env.PORT || 8001
 app.use(cors({
   origin: [
     'http://localhost:3000', 
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004',
     'https://emy-by.vercel.app',
   ],
   credentials: true,
